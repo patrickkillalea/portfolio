6cc3909c7a90ac547bb4379e8e28b331abb65e83
patrickkillalea <killalea.patrick@gmail.com>
2015-10-26 22:51:39 +0000

    added php file

---
+++ index.php
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1345\cocoasubrtf380
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red255\green255\blue255;\red148\green6\blue75;
+\red19\green36\blue126;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl336
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?php \cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 include_once\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 "home.html"\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 ); ?>}
